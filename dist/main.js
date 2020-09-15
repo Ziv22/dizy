7a46eedc8ebbf032efa0af9b1a094a595ad8d3cf
Idan Shalem <idanshalem25@gmail.com>
2020-09-15 15:00:43 +0300

    added load-page functionality

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,12 @@
 const user = new User()
+const render = new render()
 
+const loadPage = function() {
+    render.renderContent('#log-in-template')
+}
+
+$('#content').on('click', '#sign-up', function() {
+    render.renderContent('#sign-up-template')
+})
+
+loadPage()
