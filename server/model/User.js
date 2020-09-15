9f862b2a9a5857e67e46fead36822afe736495db
Ziv22 <ziv.itzh@gmail.com>
2020-09-15 16:25:10 +0300

    Deleted Dummy data

--- server/model/User.js
+++ server/model/User.js
@@ -28,27 +28,3 @@ const UserSchema = new Schema({
 const User = mongoose.model("User", UserSchema)
 
 module.exports = User
-
-const newUser = {
-    name: "Dizy",
-    address: {
-        country: "Israel",
-        city: "Tel-aviv",
-        street:"Alenby",
-        number: "918",
-        lat: 32.062916,
-        lng: 34.773433
-    },
-    contactDetails: {
-        phone:"0524355700",
-        email:"yonatanbenezra1@gmail.com"
-     },
-    password: "DizyYo",
-    interests: [], //reference to: activity-ids
-    activites:{
-        creator:[], //reference to: activity-ids
-        participant: [] //reference to: activity-ids
-}
-}
-const dummyUser = new User(newUser)
-// dummyUser.save()
