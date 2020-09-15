ecc4a2ce4a67349a4df2855432805cdf88614968
ziv221 <ziv.itzh@gmail.com>
2020-09-15 15:09:38 +0300

    added utils file and worked on the put router

--- server/routes/Activity.js
+++ server/routes/Activity.js
@@ -1,12 +1,29 @@
 const   express     = require("express"),
         router      = express.Router(),
-        Activity    = require("../model/Activity") 
+        Activity    = require("../model/Activity"),
+        User        = require("../model/User"),
+        utils       = require("./utils")
 
 router.post('/activity', async (req, res) =>{
     const   activity      = new Activity(req.body),
             saveActivity  = await activity.save()
-            
+
     res.send(saveActivity)
+})
+
+router.put('/activity/:userId/:activityId', async (req ,res) =>{
+    try{
+        const   userId      = req.params.userId,
+                activityId  = req.params.activityId
+
+        const   foundUser   = utils.findUserById(userId)
+        foundUser.activites.participant.push(activityId)
+        const updatedUser = await foundUser.save()
+        res.send(updatedUser)
+    }
+    catch(err){
+        res.send(err)
+    }
 })
 
 router.get('/activity', (req, res)=>{
