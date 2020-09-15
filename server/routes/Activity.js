bd827db16e3df6aa9fed0f155ccef4856ca603db
ziv221 <ziv.itzh@gmail.com>
2020-09-15 12:01:44 +0300

    set up activity post route

--- server/routes/Activity.js
+++ server/routes/Activity.js
@@ -2,6 +2,12 @@ const   express     = require("express"),
         router      = express.Router(),
         Activity    = require("../model/Activity") 
 
+router.post('/activity', async (req, res) =>{
+    const   activity      = new Activity(req.body),
+            saveActivity  = await activity.save()
+    res.send(saveActivity)
+})
+
 router.get('/activity', (req, res)=>{
     res.send("Welcome to Activity route")
 })
